5cd8b4f3664bd723cf781bebc999de66a1239bb8
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-09-23 15:07:55 +0500

    create some parser functionality with test

--- data/parser.py
+++ data/parser.py
@@ -1,3 +1,5 @@
+import logging
+import pandas as pd
 from settings import Settings
 
 
@@ -7,4 +9,24 @@ class Parser:
         self.settings = settings
 
     def parse(self, raw: dict) -> dict:
-        return raw
+        return {detail: self.parse_detail(detail, html_data)
+                for detail, html_data in raw.items()}
+
+    def parse_detail(self, detail: str, html_data: str) -> list:
+        tables = pd.read_html(io=html_data)
+        if not len(tables):
+            logging.info(f'{detail} data not found!')
+            return []
+
+        df = tables[0]
+        print(df)
+
+        # get column without nan
+        col = 'Detail'
+
+        # get index of not null elements in col
+        dind = df.index[df[col].notnull()].tolist()
+        dind.append(df.index[-1])
+
+        return ['data']
+
---
+++ test/data/test_parser.py
@@ -0,0 +1,95 @@
+from settings import Settings
+from data.parser import Parser
+
+html_text = """
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <th>Detail</th>
+    <th>Status</th>
+    <th>Part_of</th>
+    <th>Product</th>
+    <th>Amount</th>
+  </tr>
+  <tr>
+    <td>Pin 193</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Active</td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td>Engine</td>
+    <td>Auto</td>
+    <td>8</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td>Wheel</td>
+    <td>Moto</td>
+    <td>4</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Archive</td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td>Form</td>
+    <td>Stamp</td>
+    <td>14</td>
+  </tr>
+  <tr>
+    <td>Pin 205</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Active</td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td>Hull</td>
+    <td>Boat</td>
+    <td>256</td>
+  </tr>
+</table>
+</body>
+</html>
+"""
+
+def test_parser():
+    settings = Settings(r'.\test\settings.json')
+    assert settings.status
+
+    parser = Parser(settings)
+    assert parser.parse({'Pin 193': html_text})
