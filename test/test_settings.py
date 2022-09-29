575246cb548b22953be58f957d37d3c27c72a657
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-09-29 16:45:40 +0500

    Update test_settings.py
    see last 2 commits

--- test/test_settings.py
+++ test/test_settings.py
@@ -6,6 +6,6 @@ def test_settings():
     assert s.status
     assert s.source.file == ".\\test\\source.xlsx"
     assert s.source.sheet == "Лист1"
-    assert s.source.column == 2
+    assert s.source.column == 0
     assert s.result.file == "result.xlsx"
-    assert s.site.url == "localhost:8080"
+    assert s.site.url == "http://127.0.0.1:8000/"
